cadd358835b37d683626ed04620403e4330d6f37
singlacodes <shivamsinglatechnical@gmail.com>
2026-02-08 16:14:41 +0530

    added like, comment, delete comment, and edit caption post controllers and routes

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -81,3 +81,121 @@ export const getAllPosts = TryCatch(async (req, res) => {
 
   res.json({ posts, reels });
 }); 
+
+export const likeUnlikePost = TryCatch(async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  if (!post)
+    return res.status(404).json({
+      message: "No Post with this id",
+    });
+
+  if (post.likes.includes(req.user._id)) {
+    const index = post.likes.indexOf(req.user._id);
+
+    post.likes.splice(index, 1);
+
+    await post.save();
+
+    res.json({
+      message: "Post Unlike",
+    });
+  } else {
+    post.likes.push(req.user._id);
+
+    await post.save();
+
+    res.json({
+      message: "Post liked",
+    });
+  }
+});
+export const commentonPost = TryCatch(async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  if (!post)
+    return res.status(404).json({
+      message: "No Post with this id",
+    });
+
+  post.comments.push({
+    user: req.user._id,
+    name: req.user.name,
+    comment: req.body.comment,
+  });
+
+  await post.save();
+
+  res.json({
+    message: "Comment Added",
+  });
+});
+
+export const deleteComment = TryCatch(async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  if (!post)
+    return res.status(404).json({
+      message: "No Post with this id",
+    });
+
+
+  if (!req.query.commentId)
+    return res.status(404).json({
+      message: "Please give comment id",
+    });
+
+  const commentIndex = post.comments.findIndex(
+    (item) => item._id.toString() === req.query.commentId.toString()
+  );
+
+  if (commentIndex === -1) {
+    return res.status(400).json({
+      message: "Comment not found",
+    });
+  }
+
+  const comment = post.comments[commentIndex];
+
+  if (
+    post.owner.toString() === req.user._id.toString() ||
+    comment.user.toString() === req.user._id.toString()
+  ) {
+    post.comments.splice(commentIndex, 1);
+
+    await post.save();
+
+    return res.json({
+      message: "Comment deleted",
+    });
+  } else {
+    return res.status(400).json({
+      message: "Yor are not allowed to delete this comment",
+    });
+  }
+});
+
+
+export const editCaption = TryCatch(async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  if (!post)
+    return res.status(404).json({
+      message: "No Post with this id",
+    });
+
+  if (post.owner.toString() !== req.user._id.toString())
+    return res.status(403).json({
+      message: "You are not owner of this post",
+    });
+
+  post.caption = req.body.caption;
+
+  await post.save();
+
+  res.json({
+    message: "post updated",
+  });
+});
+
+
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
-import { deletePost, getAllPosts, newPost } from "../controllers/postControllers.js";
+import { commentonPost, deleteComment, deletePost, editCaption, getAllPosts, likeUnlikePost, newPost } from "../controllers/postControllers.js";
 import uploadFile from "../middlewares/multer.js";
 
 const router = express.Router();
 router.post("/new",isAuth,uploadFile,newPost);
 router.delete("/:id",isAuth,deletePost);
 router.get("/all",isAuth,getAllPosts);
+router.post("/like/:id", isAuth, likeUnlikePost);
+router.post("/comment/:id",isAuth,commentonPost);
+router.delete("/comment/:id", isAuth, deleteComment);
+router.put("/:id",isAuth,editCaption);
 export default router;
