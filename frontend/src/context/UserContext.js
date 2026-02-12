4585f2992910d7868e80f84c7660814a2b2d6f5b
singlacodes <shivamsinglatechnical@gmail.com>
2026-02-12 23:28:20 +0530

    refactor UserContext to manage user authentication and state; add Account page

--- frontend/src/context/UserContext.js
+++ frontend/src/context/UserContext.js
@@ -5,13 +5,95 @@ import toast, { Toaster } from "react-hot-toast";
 const UserContext = createContext();
 
 export const UserContextProvider = ({ children }) => {
-    return(
-        <UserContext.Provider value={{user:"xyz"}}>
-            {children}
-        </UserContext.Provider>
-    )
-}
+  const [user, setUser] = useState([]);
+  const [isAuth, setIsAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
+  async function registerUser(formdata, navigate, fetchPosts) {
+    setLoading(true);
+    try {
+      const { data } = await axios.post("/api/auth/register", formdata);
 
+      toast.success(data.message);
+      setIsAuth(true);
+      setUser(data.user);
+      navigate("/");
+      setLoading(false);
+      fetchPosts();
+    } catch (error) {
+      toast.error(error.response.data.message);
+      setLoading(false);
+    }
+  }
 
+  async function loginUser(email, password, navigate, fetchPosts) {
+    setLoading(true);
+    try {
+      const { data } = await axios.post("/api/auth/login", {
+        email,
+        password,
+        navigate,
+      });
+
+      toast.success(data.message);
+      setIsAuth(true);
+      setUser(data.user);
+      navigate("/");
+      setLoading(false);
+      fetchPosts();
+    } catch (error) {
+      toast.error(error.response.data.message);
+      setLoading(false);
+    }
+  }
+
+  async function fetchUser() {
+    try {
+      const { data } = await axios.get("/api/user/me");
+
+      setUser(data);
+      setIsAuth(true);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsAuth(false);
+      setLoading(false);
+    }
+  }
+
+  async function logoutUser(navigate) {
+    try {
+      const { data } = await axios.get("/api/auth/logout");
+
+      if (data.message) {
+        toast.success(data.message);
+        setUser([]);
+        setIsAuth(false);
+        navigate("/login");
+      }
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  }
+  useEffect(() => {
+    fetchUser();
+  }, []);
+  return (
+    <UserContext.Provider
+      value={{
+        loginUser,
+        isAuth,
+        setIsAuth,
+        user,
+        setUser,
+        loading,
+        logoutUser,
+        registerUser,
+      }}
+    >
+      {children}
+      <Toaster />
+    </UserContext.Provider>
+  );
+};
 
 export const UserData = () => useContext(UserContext);
