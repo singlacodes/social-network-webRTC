f9c720e7383de2489b5f7af7ffbf409013c8f78a
singlacodes <shivamsinglatechnical@gmail.com>
2026-02-23 19:12:10 +0530

    refactor server setup and add website reload functionality

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,39 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./database/db.js";
+import { connectDb } from "./database/db.js";
 import cloudinary from "cloudinary";
 import cookieParser from "cookie-parser";
-import userRoutes from "./routes/userRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
-import postRoutes from "./routes/postRoutes.js";
-import messageRoutes from "./routes/messageRoutes.js";
+import { Chat } from "./models/ChatModel.js";
+import { isAuth } from "./middlewares/isAuth.js";
+import { User } from "./models/userModel.js";
+import { app, server } from "./socket/socket.js";
+import path from "path";
+import axios from 'axios';
 
-dotenv.config();    
+const url = `https://mern-social-3e3m.onrender.com`;
+const interval = 30000;
 
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
+dotenv.config();
 
 cloudinary.v2.config({
   cloud_name: process.env.Cloudinary_Cloud_Name,
@@ -17,24 +41,32 @@ cloudinary.v2.config({
   api_secret: process.env.Cloudinary_Secret,
 });
 
-const app = express();
+//using middlewares
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-    res.send("working sir");
-}); 
+const port = process.env.PORT;
+// importing routes
+import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
 
-
-// using routes
+//using routes
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/messages", messageRoutes);
 
+const __dirname = path.resolve();
 
-const PORT =process.env.PORT;  
-app.listen(PORT, () => {
-    console.log(`✅Server is running on  http://localhost:${PORT}`);
-    connectDB();
-    }); 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+  connectDb();
+});
